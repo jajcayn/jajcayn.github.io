4f2dbc7a5c5fa5dae757b93a634da6f0f90a3bbc
Nikola Jajcay <code@nikolajajcay.eu>
2026-06-15 15:21:25 +0200

    rebuilding site Mon Jun 15 15:21:25 CEST 2026

--- js/dark.js
+++ js/dark.js
@@ -1,23 +1,24 @@
 var toggle = document.getElementById("dark-mode-toggle");
+var moonIcon = document.getElementById("icon-moon");
+var sunIcon = document.getElementById("icon-sun");
 var darkTheme = document.getElementById("dark-mode-theme");
 var savedTheme = localStorage.getItem("dark-mode-storage") || "light";
 setTheme(savedTheme);
 
-toggle.addEventListener("click", () => {
-    if (toggle.className === "nav-item nav-link far fa-moon") {
-        setTheme("dark");
-    } else if (toggle.className === "nav-item nav-link fas fa-sun") {
-        setTheme("light");
-    }
+toggle.addEventListener("click", function() {
+    var current = localStorage.getItem("dark-mode-storage") || "light";
+    setTheme(current === "light" ? "dark" : "light");
 });
 
 function setTheme(mode) {
     localStorage.setItem("dark-mode-storage", mode);
     if (mode === "dark") {
         darkTheme.disabled = false;
-        toggle.className = "nav-item nav-link fas fa-sun";
-    } else if (mode === "light") {
+        moonIcon.style.display = "none";
+        sunIcon.style.display = "";
+    } else {
         darkTheme.disabled = true;
-        toggle.className = "nav-item nav-link far fa-moon";
+        moonIcon.style.display = "";
+        sunIcon.style.display = "none";
     }
 }
